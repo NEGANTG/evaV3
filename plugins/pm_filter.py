6e9d2d86b768ae33abd9442f8b85f2fbce626c58
NEGANTG <90514589+NEGANTG@users.noreply.github.com>
2021-11-12 18:51:52 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -723,5 +723,7 @@ async def advantage_spell_chok(msg):
     a = await msg.reply("I couldn't find anything related to that\nDid you mean any one of these?", reply_markup=InlineKeyboardMarkup(btn))
     await asyncio.sleep(60)
     await a.delete()
-
-
+    await client.delete_messages(
+    chat_id=chat_id,
+    message_ids=message.message_id
+)
